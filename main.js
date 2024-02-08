e0e9e3bb19b8257b7707e01d3423b68138d1bf01
PeiHsiuLu <144085514+PeiHsiuLu@users.noreply.github.com>
2024-02-09 00:34:37 +0800

    WEB

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollToHome() {
+    var homeDiv = document.querySelector('.home');
+    homeDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAboutMe() {
+    var aboutMeDiv = document.querySelector('.about-me');
+    aboutMeDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
